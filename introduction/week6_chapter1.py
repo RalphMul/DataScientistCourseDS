919cdbd2b919bf360a8b2715fe2f16d7c7bd9190
RalphMul <70332927+RalphMul@users.noreply.github.com>
2020-08-27 17:11:30 +0200

    Add files via upload

---
+++ introduction/week6_chapter1.py
@@ -0,0 +1,8 @@
+"Week 6 chapter 1"
+def thing():
+    print ('Hello')
+    print ('Fun')
+
+thing()
+print('zip')
+thing()
---
+++ introduction/week7_chapter4_2.py
@@ -0,0 +1,9 @@
+# looping to a set
+smallest_so_far = 99999999
+print ("before", smallest_so_far)
+for the_num in [9, 41, 12, 3, 74, 15] :
+    if the_num < smallest_so_far :
+        smallest_so_far = the_num
+    print (smallest_so_far, the_num)
+
+print ('After', smallest_so_far)
